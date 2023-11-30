df36493b358551a193d594a4c8ef2264e1afb419
Dara <moskalenko.dara@mail.ru>
2023-11-30 20:07:58 +0300

    Добавлена функция генерации действительного числа в заданном диапазоне

--- Squirrel2/Squirrel2.h
+++ Squirrel2/Squirrel2.h
@@ -1,1 +1,12 @@
 #pragma once
+#include <cstdlib>
+
+/// <summary>
+/// Generate random float in given range
+/// </summary>
+/// <param name="min">lower bound</param>
+/// <param name="max">upper bound</param>
+/// <returns>random float in range from 'min' to 'max'</returns>
+float rand_float(float min, float max) {
+	return min + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (max - min)));
+}
